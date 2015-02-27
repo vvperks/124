ac28ee63f154cb54bf0293516cb7245d6d7b4270
Eric Perkins <ericperkins@DN51qo6q.SUNet>
2015-02-26 22:43:32 -0800

    Switched one dev set/test set pair, and added folders that will be used for submission

---
+++ to_submit/corpus/corp.py
@@ -0,0 +1,18 @@
+###DEV SET###
+1) "Cuando se accede al ordenador como un administrador, pueden añadirse otros usuarios, configurar Usuarios Múltiples de Mac OS X, cambiar determinados ajustes del sistema y, en general, disponer de mayor acceso al sistema."
+2) "Su color, profundidad, y movimiento ayudan a guiarlo con sus deberes, el Finder y el Dock permiten acceder al ordenador y conectarse a redes cómodamente."
+3) "A lo largo del manual, observará preguntas como la que aparece a continuación."
+4) " El número indica los mensajes pendientes de lectura."
+5) "Para ordenar los ítems en la barra de herramientas, arrástrelos."
+6) "Podrá configurarlo fácilmente añadiendo cuentas de usuario que requieran el uso de nombres de usuario y contraseñas."
+7) "Para mayor seguridad, inicie la sesión con un usuario distinto al administrador"
+8) "Este icono rojo indica que está conectado."
+9) "Envíe notas cortas a sus compañeros o mande fotografías de sus hijos a otras personas."
+10) "Ésta es la impresora por omisión."
+
+###TEST SET###
+1) "Para visualizar una imagen incluida en un mensaje de esa persona, arrástrela hasta aquí."
+2) "Si no encuentra la información, búsquela en la aplicación de correo o en el navegador web."
+3) "Escriba el nombre y la contraseña exactamente igual que si configurase el Mac OS X para múltiples usuarios."
+4) "Cuando se inicia la sesión como administrador se dispone de mayores privilegios de acceso, por lo que no conviene abandonar el ordenador."
+5) "Haga clic aquí para visualizar las preferencias del sistema."
---
+++ to_submit/google/google_trans.py
@@ -0,0 +1,14 @@
+1) "Para visualizar una imagen incluida en un mensaje de esa persona, arrástrela hasta aquí."
+Google Translation: "To display an image in a message to that person, drag it here."
+
+2) "Si no encuentra la información, búsquela en la aplicación de correo o en el navegador web."
+Google Translation: "Can not find the information, look in the mail application or web browser."
+
+3) "Escriba el nombre y la contraseña exactamente igual que si configurase el Mac OS X para múltiples usuarios."
+Google Translation: "Enter the name and password exactly as if configurase the Mac OS X for multiple users."
+
+4) "Cuando se inicia la sesión como administrador se dispone de mayores privilegios de acceso, por lo que no conviene abandonar el ordenador."
+Google Translation: "When you log in as administrator it has greater access privileges, so you should not leave the computer."
+
+5) "Haga clic aquí para visualizar las preferencias del sistema."
+Google Translation: "Click here to display the system preferences."
